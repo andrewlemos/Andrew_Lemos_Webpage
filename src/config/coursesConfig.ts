0e9704db996ddf13839b2bdcdb220a23d73cd04f
Andrew Lemos <156254579+andrewlemos@users.noreply.github.com>
2026-06-29 11:19:33 -0300

    link de subdominio para academia de artes

--- src/config/coursesConfig.ts
+++ src/config/coursesConfig.ts
@@ -5,9 +5,9 @@
  *   - true: Qualquer visitante ou visitante logado pode acessar o link externo.
  *   - false: Somente o administrador autenticado (andrewfmlemos@gmail.com) pode acessar.
  */
-export const ENABLE_COURSES_PUBLIC_ACCESS = false;
+export const ENABLE_COURSES_PUBLIC_ACCESS = true;
 
 /**
  * Endereço do novo link externo para o sistema de cursos online.
  */
-export const COURSES_EXTERNAL_URL = "https://plataforma-de-cursos-e-apostilas-de-arte-743645073573.us-east1.run.app";
+export const COURSES_EXTERNAL_URL = "https://academiadeartes.andrewlemos.com/";
