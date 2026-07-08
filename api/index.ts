07cd5bac6a2862c237276d13ab3a205d78333e33
Andrew Lemos <156254579+andrewlemos@users.noreply.github.com>
2026-07-08 17:07:36 -0300

    ajuste webhook melhor envio

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,13 @@
+import express from "express";
 import logicApp from "../server/server-logic";
 
-export default logicApp;
+const app = express();
+
+app.all("*", (req, res, next) => {
+  // Resiliently resolve ESM default export or CJS export
+  const resolvedApp = (logicApp as any).default || logicApp;
+  return resolvedApp(req, res, next);
+});
+
+export default app;
 
