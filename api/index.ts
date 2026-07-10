66992d65e77362f4369370d3c2dcb8397466bb35
Andrew Lemos <156254579+andrewlemos@users.noreply.github.com>
2026-07-09 22:18:56 -0300

    restauração para dia 7

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,4 @@
-import express from "express";
 import logicApp from "../server/server-logic";
 
-const app = express();
-
-app.all("*", (req, res, next) => {
-  // Resiliently resolve ESM default export or CJS export
-  const resolvedApp = (logicApp as any).default || logicApp;
-  return resolvedApp(req, res, next);
-});
-
-export default app;
+export default logicApp;
 
